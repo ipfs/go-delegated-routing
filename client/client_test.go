6bb51637b69097bb362803837f1fea692aa3ce59
Petar Maymounkov <petarm@gmail.com>
2021-10-20 16:49:19 -0700

    fix client test

--- client/client_test.go
+++ client/client_test.go
@@ -2,22 +2,30 @@ package client
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt"
 	"strings"
 	"testing"
+
+	"github.com/multiformats/go-multiaddr"
 )
 
 func TestParseFindProvsResp(t *testing.T) {
 
+	id := "QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N"
+	ma1 := base64.RawStdEncoding.EncodeToString(multiaddr.StringCast("/ip4/7.7.7.7/tcp/4242/p2p/" + id).Bytes())
+	ma2 := base64.RawStdEncoding.EncodeToString(multiaddr.StringCast("/ip4/8.8.8.8/tcp/4242/p2p/" + id).Bytes())
+
 	// These multiaddrs are incorrect, they're base64padded text multiaddrs instead of base64-unpadded byte multiaddrs
-	respStr := `{"tag" : "get-p2p-provide",
+	respStr := fmt.Sprintf(`{"tag" : "get-p2p-provide",
 "payload" : {
 	"peers" : [
-		{"/" : {"bytes" : "L2lwNC80Ni4xNy45Ni45OS90Y3AvMzA2MDIvcDJwL1FtVzltNTdhaUJESEFrS2o5bm1GU0VuN1pxcmNGMWZaUzRiaXBzVENIYnVyZWk="}},
-		{"/" : {"bytes" : "L2lwNC80Ni4xNy45Ni45OS90Y3AvMzA2MDIvcDJwL1FtVzltNTdhaUJESEFrS2o5bm1GU0VuN1pxcmNGMWZaUzRiaXBzVENIYnVyZWk="}}
+		{"/" : {"bytes" : %q}},
+		{"/" : {"bytes" : %q}}
 	]
 }
 }
-`
+`, ma1, ma2)
 	r := strings.NewReader(respStr)
 	ch := make(chan FindProvidersAsyncResult, 2)
 	processFindProvidersAsyncResp(context.Background(), ch, r)
@@ -28,5 +36,16 @@ func TestParseFindProvsResp(t *testing.T) {
 	if p1.Err != nil {
 		t.Fatal(p1.Err)
 	}
-	t.Log(p1.AddrInfo)
+	if len(p1.AddrInfo) != 2 {
+		t.Fatalf("unexpected length")
+	}
+	if p1.AddrInfo[0].ID.String() != id {
+		t.Errorf("got %v, expecting %v", p1.AddrInfo[0].ID.String(), id)
+	}
+	if len(p1.AddrInfo[0].Addrs) != 1 || p1.AddrInfo[0].Addrs[0].String() != "/ip4/7.7.7.7/tcp/4242" {
+		t.Errorf("unexpecting address")
+	}
+	if len(p1.AddrInfo[1].Addrs) != 1 || p1.AddrInfo[1].Addrs[0].String() != "/ip4/8.8.8.8/tcp/4242" {
+		t.Errorf("unexpecting address")
+	}
 }
