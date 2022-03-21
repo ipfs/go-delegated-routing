2a898de5f8069af34333830e0452f2dca1ff4a93
Petar Maymounkov <petarm@gmail.com>
2022-03-21 11:55:32 -0700

    add regression stress test for memory and goroutines

--- test/clientserver_test.go
+++ test/clientserver_test.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"net/http/httptest"
 	"os"
+	"runtime"
 	"testing"
+	"time"
 
 	"github.com/ipfs/go-cid"
 	"github.com/ipfs/go-delegated-routing/client"
@@ -20,34 +22,67 @@ func TestClientServer(t *testing.T) {
 	// start a server
 	s := httptest.NewServer(server.FindProvidersAsyncHandler(testFindProvidersAsyncFunc))
 	defer s.Close()
+
 	// start a client
 	q, err := proto.New_DelegatedRouting_Client(s.URL, proto.DelegatedRouting_Client_WithHTTPClient(s.Client()))
 	if err != nil {
 		t.Fatal(err)
 	}
 	c := client.NewClient(q)
+
 	// verify result
 	h, err := multihash.Sum([]byte("TEST"), multihash.SHA3, 4)
 	if err != nil {
 		t.Fatal(err)
 	}
-	infos, err := c.FindProviders(context.Background(), cid.NewCidV1(cid.Libp2pKey, h))
-	if err != nil {
-		t.Fatal(err)
+
+	ngoStart, allocStart := snapUtilizations()
+	fmt.Printf("start: goroutines=%d allocated=%d\n", ngoStart, allocStart)
+
+	const N = 1e4
+	for i := 0; i < N; i++ {
+		infos, err := c.FindProviders(context.Background(), cid.NewCidV1(cid.Libp2pKey, h))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(infos) != 1 {
+			t.Fatalf("expecting 1 result, got %d", len(infos))
+		}
+		if infos[0].ID != testAddrInfo.ID {
+			t.Errorf("expecting %#v, got %#v", testAddrInfo.ID, infos[0].ID)
+		}
+		if len(infos[0].Addrs) != 1 {
+			t.Fatalf("expecting 1 address, got %d", len(infos[0].Addrs))
+		}
+		if !infos[0].Addrs[0].Equal(testAddrInfo.Addrs[0]) {
+			t.Errorf("expecting %#v, got %#v", testAddrInfo.Addrs[0], infos[0].Addrs[0])
+		}
+		// fmt.Println(infos)
 	}
-	if len(infos) != 1 {
-		t.Fatalf("expecting 1 result, got %d", len(infos))
+	ngoEnd, allocEnd := snapUtilizations()
+	fmt.Printf("end: goroutines=%d allocated=%d\n", ngoEnd, allocEnd)
+	fmt.Printf("diff: goroutines=%d allocated=%d\n", ngoEnd-ngoStart, allocEnd-allocStart)
+
+	// we have ran this test with increasing number of iterations (N = 1e3, 1e4, 1e5, 1e6, 1e7)
+	// in all cases, the number of excess goroutines and memory allocation does not increase with the number of test iterations.
+	// we have observed that excess goroutines (ngoEnd-ngoStart) always equal 3, and
+	// excess memory allocation (allocEnd-allocStart) closely varies around 290K.
+	// these observations are codified in the regression checks below.
+
+	if ngoEnd-ngoStart > 3 {
+		t.Errorf("goroutine leakage")
 	}
-	if infos[0].ID != testAddrInfo.ID {
-		t.Errorf("expecting %#v, got %#v", testAddrInfo.ID, infos[0].ID)
+	if allocEnd-allocStart > 300e3 {
+		t.Errorf("memory leakage")
 	}
-	if len(infos[0].Addrs) != 1 {
-		t.Fatalf("expecting 1 address, got %d", len(infos[0].Addrs))
-	}
-	if !infos[0].Addrs[0].Equal(testAddrInfo.Addrs[0]) {
-		t.Errorf("expecting %#v, got %#v", testAddrInfo.Addrs[0], infos[0].Addrs[0])
-	}
-	fmt.Println(infos)
+}
+
+func snapUtilizations() (numGoroutines int, alloc uint64) {
+	runtime.GC()
+	time.Sleep(time.Second)
+	var ms runtime.MemStats
+	runtime.ReadMemStats(&ms)
+	return runtime.NumGoroutine(), ms.Alloc
 }
 
 const (
